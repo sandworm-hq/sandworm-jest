745800904322a80f799e3c8c6b91fb7ee5184b5d
Gabi Dobocan <gabi@telepat.io>
2022-09-29 22:40:52 +0300

    feat: update `sandworm-utils` to v1.4.0

--- setup/index.js
+++ setup/index.js
@@ -1,23 +1,7 @@
-const path = require('path');
 const {
   recorder: {recordSandwormActivity},
-  files: {loadConfig},
   logger,
 } = require('sandworm-utils');
-
-const appPath = process.env.SANDWORM_APP_PATH || path.join(__dirname, '..', '..', '..');
-let config;
-
-try {
-  config = loadConfig(appPath);
-  if (config) {
-    logger.log('Config loaded');
-  }
-} catch (error) {
-  logger.log('Error loading config:', error.message);
-}
-
-globalThis.SANDWORM_CONFIG = config;
 
 module.exports = async () => {
   logger.log('Starting listener...');
